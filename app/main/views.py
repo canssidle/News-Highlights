052d43c493b6e4981e28b6fb1822988ae28ed7d0
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-14 15:11:14 +0300

    creation of the main route

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,11 @@ from flask import render_template
 from . import main
 from ..requests import get_news
 
-# @main.route("/")
-# def index():
-#     # title = "welcome home"
+@main.route("/")
+def index():
+    # title = "welcome home"
    
-#     return render_template("index.html")
+    return render_template("index.html")
     
 # @main.route("/category/<string:category>")  
 # def categories(category):
