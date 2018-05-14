d3e4a0b57f5f93761af81d8cf585d0f7a67b06a1
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-14 15:07:49 +0300

    creating the production and developer class

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
     NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# class ProdConfig(Config):
-#     pass
+class ProdConfig(Config):
+    pass
 
-# class DevConfig(Config):
-#     DEBUG = True
+class DevConfig(Config):
+    DEBUG = True
 
-# config_options = {
-#     'development': DevConfig,
-#     'production': ProdConfig
-# }
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
