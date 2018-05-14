4fdfe203b13b554c819582eccc20885912dc5857
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-14 15:10:07 +0300

    creating the app configurations

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -9,17 +9,17 @@ from . import views ,errors
 def create_app(config_name):
     app = Flask(__name__)
 
-#     #Creating the app configurations
+    #Creating the app configurations
 
-#     app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
 
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-#     # Registering the blueprint
+    # Registering the blueprint
 
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-#     return app
+    return app
