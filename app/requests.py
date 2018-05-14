7900c85ab989b2c7a5a6e87e130cdeda7f3866f4
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-14 15:03:40 +0300

    defining the get news function

--- app/requests.py
+++ app/requests.py
@@ -10,8 +10,8 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
 
-# def get_news(category):
-#     get_news_url = base_url.format(category,api_key)
+def get_news(category):
+    get_news_url = base_url.format(category,api_key)
 
 #     mydata = []
 
