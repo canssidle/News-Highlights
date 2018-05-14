cc1fee87ce584da930171cf87aea864a78864215
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-14 15:02:58 +0300

    defining the api ket

--- app/requests.py
+++ app/requests.py
@@ -2,13 +2,13 @@ import urllib.request, json
 from .models import News
 
 
-# api_key = None
-# base_url = None
+api_key = None
+base_url = None
 
-# def configure_request(app):
-#     global api_key , base_url
-#     api_key = app.config['NEWS_API_KEY']
-#     base_url = app.config['NEWS_API_BASE_URL']
+def configure_request(app):
+    global api_key , base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
 
 # def get_news(category):
 #     get_news_url = base_url.format(category,api_key)
